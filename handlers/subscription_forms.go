4bf82ae4c64d18d1a7735f86c84cc9fadb4b4d1b
Ghenoo <lucasgheno600@gmail.com>
2024-02-21 12:40:50 -0300

    create subscription form

--- handlers/subscription_forms.go
+++ handlers/subscription_forms.go
@@ -1,7 +1,24 @@
 package handlers
 
-func SubscriptionHandler(w http.ResponseWritter, r *http.Request) {
-	if r.Method == http.MethodPost {
+import (
+	"fmt"
+	"form-web/controllers"
+	"net/http"
+)
 
+func SubscriptionHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		if err := r.ParseForm(); err != nil {
+			fmt.Fprint(w, "erro ao fazer parse do form: %v", err)
+			return
+		}
+
+		err := controllers.Create(r.PostForm.Get("name"), r.PostForm.Get("email"))
+		if err != nil {
+			fmt.Fprint(w, "erro ao salvar dados: %v", err)
+			return
+		}
 	}
+
+	http.ServeFile(w, r, "handlers/templates/subscription_forms.html")
 }
