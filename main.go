a040b80c74e1889689a0812a121b5d0118f83eb4
Ghenoo <lucasgheno600@gmail.com>
2024-02-21 12:46:08 -0300

    att main

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"form-web/handlers"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", handlers.SubscriptionHandler)
 
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
