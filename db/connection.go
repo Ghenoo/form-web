2f42bed483722bd0726c4d63c877d1dd67f897d1
Ghenoo <lucasgheno600@gmail.com>
2024-02-21 12:41:41 -0300

    commit connection

--- db/connection.go
+++ db/connection.go
@@ -6,11 +6,10 @@ import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func getConnection() (client *mongo.Client, ctx context.Context) {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	client, err := mongo.NewClient(optios.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
 		log.Fatal(err)
 	}
