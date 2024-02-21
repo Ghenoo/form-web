0390ec0d4f8a8b38d450f3b490736a36de794d34
Ghenoo <lucasgheno600@gmail.com>
2024-02-21 12:42:54 -0300

    att subscription

--- handlers/subscription_forms.go
+++ handlers/subscription_forms.go
@@ -9,13 +9,13 @@ import (
 func SubscriptionHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
 		if err := r.ParseForm(); err != nil {
-			fmt.Fprint(w, "erro ao fazer parse do form: %v", err)
+			fmt.Fprintf(w, "erro ao fazer parse do form: %v", err)
 			return
 		}
 
 		err := controllers.Create(r.PostForm.Get("name"), r.PostForm.Get("email"))
 		if err != nil {
-			fmt.Fprint(w, "erro ao salvar dados: %v", err)
+			fmt.Fprintf(w, "erro ao salvar dados: %v", err)
 			return
 		}
 	}
